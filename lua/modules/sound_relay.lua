bf53ad1fc98175067cde8ede4e034326905bc38e
Charlene Hoo <CharleneHoo@hotmail.com>
2026-03-10 13:30:30 +0800

    feat(sound_relay): add debug output controlled by ProxyManager.DEBUG
    - Print details when sound is ignored, when victim is detected,
      and when relaying to proxies
    - Add warning for unexpected nil view in GetProxiesByVictim

--- lua/modules/sound_relay.lua
+++ lua/modules/sound_relay.lua
@@ -2,6 +2,7 @@
 
 local ProxyManager = ProxyManager
 local PROXY_CLASS = ProxyManager.PROXY_CLASS
+local DEBUG = ProxyManager.DEBUG or false
 
 local IsValid = IsValid
 
@@ -22,6 +23,13 @@ local function Deserialize(data)
         data.Channel, data.Flags, data.DSP
 end
 
+local function DebugPrint(data, victim, proxies)
+    if DEBUG then
+        print(string.format("[SNT_SoundRelay] Relaying sound '%s' from victim %d to %d proxies",
+            data.SoundName, victim:EntIndex(), #proxies))
+    end
+end
+
 hook.Add("EntityEmitSound", "SNT_EntityEmitSound", function(data)
     local entity = data.Entity
     if not IsValid(entity) or entity:GetClass() == PROXY_CLASS then
@@ -38,6 +46,9 @@ hook.Add("EntityEmitSound", "SNT_EntityEmitSound", function(data)
     end
 
     local proxies = GetProxiesByVictim(victim)
+
+    DebugPrint(data, victim, proxies)
+
     for _, proxy in ipairs(proxies) do
         -- 原样传递声音参数
         proxy:EmitSound(
